249393149201a1f641f72aa33a54ac6e08c48ccf
Nour-AlSulais <s202271740@kfupm.edu.sa>
2026-04-15 11:15:58 +0300

    todo3

--- 7-2-restful-api/server/server.js
+++ 7-2-restful-api/server/server.js
@@ -14,13 +14,29 @@ const PORT = process.env.PORT || 5174;
 app.use(cors());              
 app.use(express.json());
 
-await connectDB(process.env.MONGO_URL);
-
+await connectDB();
 // api/songs (Read all songs)
 
 
 // api/songs (Insert song)
+app.post("/api/songs", async (req, res) => {
+  try {
+    const { title = "", artist = "", year } = req.body || {};
 
+    const created = await Song.create({
+      title: title.trim(),
+      artist: artist.trim(),
+      year
+    });
+
+    res.status(201).json(created);
+
+  } catch (err) {
+    res.status(400).json({
+      message: err.message || "Failed to create song"
+    });
+  }
+});
 // /api/songs/:id (Update song)
 
 
